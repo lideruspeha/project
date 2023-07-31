366048676b196e6b3aa9a539dd6349ba20a0a910
Mikhail <lider-uspeha@yandex.ru>
2023-07-31 14:36:33 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(pi , second , leftBorderWidth);
+
+
+// var ladaGranta = 'lada';
+// let grantaLada = 'granta';
+
+// console.log(ladaGranta , grantaLada);
+
+// var Wolcvagen = 'Golf';
+// var Bmw = 'X5';
+
+// console.log(Wolcvagen , Bmw);
+
+// let lastName = "Nastya";
+// var firstName = "Mokrushina";
+// const adRess = "Perm"
+
+// console.log(lastName , firstName , adRess);
+
+// alert('Hello World')
+
+// let answer = confirm('Вам есть 18 лет?');
+// console.log(answer);
+
+// let answer = prompt('Есть ли Вам 18 лет?' , 'Да');
+//  console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+    
+    console.log (incr++);
+    console.log (decr--);
